e62b1516b6c1690ee61458ea444170b54a71ee36
zarina20043 <zarinaseitnabi@gmail.com>
2025-05-25 19:48:38 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import HomePage from "./HomePage";
+
+export default function App() {
+  return <HomePage />;
+}
---
+++ HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif", textAlign: "center" }}>
+      <h1>Qaita.kz</h1>
+      <p>Білім – болашақтың кілті. Бұл платформада қазақтың тарихы мен мәдениетіне арналған тесттер мен сабақтар бар.</p>
+    </div>
+  );
+}
